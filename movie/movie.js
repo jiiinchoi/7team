bdbdf9e716f29ece8eaa2a81d4cde94867fdb897
yun9699 <cha23215@daum.net>
2024-01-30 17:15:53 +0900

    lyc movie 별표 초기값

--- movie/movie.js
+++ movie/movie.js
@@ -1,4 +1,3 @@
 const drawStar = (target) => {
   document.querySelector(`.star span`).style.width = `${target.value * 10}%`;
-  document.querySelector(`.star-color`).style.width = `${target.value * 10}%`;
 }
