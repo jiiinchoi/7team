f7691f7a8b666b44090d842f8fcf906a61729d74
yun9699 <cha23215@daum.net>
2024-01-30 17:06:09 +0900

    lyc movie 별 테스트

--- movie/movie.js
+++ movie/movie.js
@@ -1,3 +1,4 @@
 const drawStar = (target) => {
   document.querySelector(`.star span`).style.width = `${target.value * 10}%`;
+  document.querySelector(`.star-color`).style.width = `${target.value * 10}%`;
 }
