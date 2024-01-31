6302d9cc43dce482becdc5b394dddb8c8ac67382
yun9699 <cha23215@daum.net>
2024-01-31 11:09:21 +0900

    lyc movie 텍스트입력창 추가

--- movie/movie.js
+++ movie/movie.js
@@ -8,3 +8,11 @@ const drawStar = (target) => {
 }
 
 drawStar(document.querySelector('.star input'));
+
+window.addEventListener('resize', function() {
+  if (window.innerWidth <= 500) {
+    document.querySelector("#mycomment_tb input[type='text']").setAttribute("placeholder", "평점은 최대 500자까지 등록 가능합니다.");
+  } else {
+    document.querySelector("#mycomment_tb input[type='text']").setAttribute("placeholder", "평점은 최대 500자까지 등록 가능합니다. 영화와 상관 없는 내용은 제재를 받을 수 있습니다");
+  }
+});
