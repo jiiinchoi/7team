58a704cb9b9ea7d5e3f84cc08280862f153dbe1f
yun9699 <cha23215@daum.net>
2024-01-31 09:58:07 +0900

    lyc movie 숫자간격 0.5 조정

--- movie/movie.js
+++ movie/movie.js
@@ -2,8 +2,9 @@ const drawStar = (target) => {
   const filledStar = document.querySelector('.star span');
   const result = document.getElementById('result');
 
+  const value = (target.value / 2).toFixed(1);
   filledStar.style.width = `${target.value * 10}%`;
-  result.textContent = target.value/2;
+  result.textContent = value;
 }
 
 drawStar(document.querySelector('.star input'));
