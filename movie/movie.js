ead6f73a662b11ad114e9bae189e48d5886113ff
yun9699 <cha23215@daum.net>
2024-01-31 15:31:46 +0900

    lyc movie textarea 폰트크기 반응형으로

--- movie/movie.js
+++ movie/movie.js
@@ -1,3 +1,4 @@
+// 별점 선택관련 
 const drawStar = (target) => {
   const filledStar = document.querySelector('.star span');
   const result = document.getElementById('result');
@@ -9,54 +10,8 @@ const drawStar = (target) => {
 
 drawStar(document.querySelector('.star input'));
 
-window.addEventListener('resize', function() {
-  if (window.innerWidth <= 500) {
-    document.querySelector("#mycomment_tb input[type='text']").setAttribute("placeholder", "평점은 최대 500자까지 등록 가능합니다.");
-  } else {
-    document.querySelector("#mycomment_tb input[type='text']").setAttribute("placeholder", "평점은 최대 500자까지 등록 가능합니다. 영화와 상관 없는 내용은 제재를 받을 수 있습니다");
-  }
-});
 
-
-// const input = document.querySelector("#mycomment_tb input[type='text']");
-// const charCount = document.createElement("span");
-
-// input.addEventListener("input", () => {
-//   const textLength = input.value.length;
-//   const remainingCharacters = 500 - textLength;
-//   charCount.textContent = `(${textLength}/500) 남은 텍스트: ${remainingCharacters}`;
-
-//   charCount.style.position = "absolute";
-//   charCount.style.bottom = "-10px";
-//   charCount.style.left = "5px";
-
-//   input.parentNode.appendChild(charCount);
-// });
-
-
-
-// const input = document.querySelector("#mycomment_tb input[type='text']");
-// const charCount = document.querySelector("#my-char-count");
-
-// input.addEventListener("input", () => {
-//   const textLength = input.value.length;
-//   const remainingCharacters = 500 - textLength;
-//   charCount.textContent = `(${textLength}/500) 남은 텍스트: ${remainingCharacters}`;
-// });
-
-
-
-// const textarea = document.querySelector("#mycomment");
-// const charCount = document.querySelector("#my-char-count");
-
-// textarea.addEventListener("input", () => {
-//   const textLength = textarea.value.replace(/\s/g, "").length;
-//   const remainingCharacters = 500 - textLength;
-//   charCount.textContent = `(${textLength}/500) 남은 텍스트: ${remainingCharacters}`;
-// });
-
-// const textLength = textarea.value.length;
-
+// 텍스트카운터 관련 
 const textarea = document.querySelector("#mycomment");
 const charCount = document.querySelector("#my-char-count");
 
@@ -72,3 +27,8 @@ textarea.addEventListener("input", () => {
     charCount.textContent = "(0/500) 남은 텍스트: 500";
   }
 });
+
+// 플레이스홀더 관련 
+const isMobile = window.matchMedia("(max-width: 500px)").matches;
+const placeholderText = isMobile ? "평점은 최대 500자까지 등록 가능합니다" : "평점은 최대 500자까지 등록 가능합니다. 영화와 상관 없는 내용은 제재를 받을 수 있습니다.";
+textarea.setAttribute("placeholder", placeholderText);
