d2b493e08e07c55c70410f4d88e6a15c6ed61657
yun9699 <cha23215@daum.net>
2024-02-01 15:49:58 +0900

    lyc movie 좋아요 버튼 숫자  증감기능 수정

--- movie/movie.js
+++ movie/movie.js
@@ -32,3 +32,70 @@ textarea.addEventListener("input", () => {
 const isMobile = window.matchMedia("(max-width: 500px)").matches;
 const placeholderText = isMobile ? "평점은 최대 500자까지 등록 가능합니다" : "평점은 최대 500자까지 등록 가능합니다. 영화와 상관 없는 내용은 제재를 받을 수 있습니다.";
 textarea.setAttribute("placeholder", placeholderText);
+
+
+
+// 버튼 클릭 숫자 +1 기능
+// var increaseBtn = document.getElementById("increaseBtn");
+// var numberElement = document.getElementById("number");
+
+// var number = 0;
+
+// increaseBtn.addEventListener("click", function() {
+
+//   if (increaseBtn.textContent === "증가") {
+//     number += 1;
+//   } else {
+//     number -= 1;
+//   }
+  
+//   numberElement.textContent = number;
+  
+//   if (increaseBtn.textContent === "증가") {
+//     increaseBtn.textContent = "감소";
+//   } else {
+//     increaseBtn.textContent = "증가";
+//   }
+// });
+
+function changeCount(elementId) {
+  var likeCount = document.getElementById(elementId);
+  var currentCount = parseInt(likeCount.textContent.trim());
+
+  // 숫자 증감 로직
+  if (likeCount.classList.contains("liked")) {
+    currentCount -= 1;
+    likeCount.classList.remove("liked"); // 이미 좋아요 상태인 경우, 좋아요 취소
+  } else {
+    currentCount += 1;
+    likeCount.classList.add("liked"); // 좋아요 상태로 변경
+  }
+
+  likeCount.textContent = currentCount;
+}
+
+// 하트 아이콘 움직임 기능
+function changeIcon(icon) {
+  const likeCount = document.getElementById("like-count");
+
+  if (icon.classList.contains("fa-solid")) {
+    icon.classList.remove("fa-solid");
+    icon.classList.remove("fa-beat");
+    icon.classList.remove("LikeContent");
+    icon.style.removeProperty("--fa-animation-duration");
+
+    likeCount.innerHTML = parseInt(likeCount.innerHTML) - 1;
+  } else {
+    icon.classList.add("fa-solid");
+    icon.classList.add("fa-beat");
+    icon.classList.add("LikeContent");
+    icon.style.setProperty("--fa-animation-duration", "1s");
+
+    likeCount.innerHTML = parseInt(likeCount.innerHTML) + 1;
+
+    // Remove the "fa-beat" class after 3 seconds to stop the animation
+    setTimeout(() => {
+      icon.classList.remove("fa-beat");
+    }, 2000);
+  }
+}
